38eb125aa51b965d97858b18a6051935de5ab00b
Jing Li <jingli1524@gmail.com>
2022-06-27 23:12:27 -0400

    rework music player

--- components/MusicPlayers.tsx
+++ components/MusicPlayers.tsx
@@ -2,14 +2,14 @@ import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import { motion, AnimatePresence, useCycle } from "framer-motion";
 import classNames from "classnames";
-import AudioControls from "./AudioControls";
+import CurrentTrack from "./CurrentTrack";
 
 function MusicPlayers({ tracks }): JSX.Element {
   const [isPlaying, setIsPlaying] = useState(false);
   const [trackIndex, setTrackIndex] = useState(0);
   const [trackProgress, setTrackProgress] = useState(0);
   const [duration, setDuration] = useState(0);
-
+  const [currentTrack, setCurrentTrack] = useState();
   const { title, artist, audioSrc, durationTime } = tracks[trackIndex];
 
   // can't run Audio API on server
@@ -31,7 +31,6 @@ function MusicPlayers({ tracks }): JSX.Element {
   };
 
   const intervalRef = useRef(0);
-  const isReady = useRef(false);
 
   const toPrevTrack = () => {
     if (trackIndex - 1 < 0) {
@@ -61,38 +60,6 @@ function MusicPlayers({ tracks }): JSX.Element {
     }, [1000]);
   };
 
-  useEffect(() => {
-    if (isPlaying) {
-      audioRef?.current?.play();
-      startTimer(); //keep track of the progress
-    } else {
-      clearInterval(intervalRef.current);
-      audioRef?.current?.pause();
-    }
-  }, [isPlaying]);
-
-  useEffect(() => {
-    // Pause and clean up on unmount
-    return () => {
-      audioRef?.current?.pause();
-      clearInterval(intervalRef.current);
-    };
-  }, []);
-
-  useEffect(() => {
-    audioRef.current.pause();
-
-    audioRef.current = new Audio(audioSrc);
-    setTrackProgress(audioRef.current.currentTime);
-    if (isReady.current) {
-      audioRef.current.play();
-      setIsPlaying(true);
-      startTimer();
-    } else {
-      isReady.current = true;
-    }
-  }, [trackIndex]);
-
   //progress bar dragging
   const onScrub = (value) => {
     //clear any timers that are already running
@@ -110,7 +77,10 @@ function MusicPlayers({ tracks }): JSX.Element {
 
   const handlePlay = (index) => {
     console.log("clicked index", index);
-    setIsPlaying(true);
+    setIsPlaying((prev) => {
+      console.log(prev);
+      return prev === index ? false : true;
+    });
   };
 
   return (
@@ -129,45 +99,21 @@ function MusicPlayers({ tracks }): JSX.Element {
         </div> */}
         <motion.div className=" w-[60%]">
           {/* button control */}
-          <div className="p-8 mx-20">
-            <div className="flex flex-col items-center mt-5 mb-10 track-info">
-              <h2 className="text-xl font-bold text-white/80 font-raleway">
-                {title}
-              </h2>
-              <h3 className="text-xl font-bold text-white/80 font-raleway">
-                {artist}
-              </h3>
-            </div>
-            <AudioControls
-              isPlaying={isPlaying}
-              onPrevClick={toPrevTrack}
-              onNextClick={toNextTrack}
-              onPlayPauseClick={setIsPlaying}
-            />
-            <div>
-              <input
-                type="range"
-                value={trackProgress}
-                step="0.1"
-                min="0"
-                max={duration ? duration : `${duration}`}
-                className=" progress w-[100%] mt-10 rounded-lg bg-red-300 inline"
-                onChange={(e) => onScrub(e.target.value)}
-                onMouseUp={onScrubEnd}
-                onKeyUp={onScrubEnd}
-                // style={{ background: trackStyling }}
-              />
-              {audioRef?.current?.duration ? (
-                <p className="block float-right text-xs font-bold text-white/80 font-PT_sans">
-                  {`${calculateTime(
-                    audioRef?.current?.currentTime
-                  )}/-${durationTime}`}
-                </p>
-              ) : (
-                ""
-              )}
-            </div>
-          </div>
+          <CurrentTrack
+            audioRef={audioRef}
+            tracks={tracks}
+            setIsPlaying={setIsPlaying}
+            toPrevTrack={toPrevTrack}
+            toNextTrack={toNextTrack}
+            isPlaying={isPlaying}
+            onScrubEnd={onScrubEnd}
+            onScrub={onScrub}
+            calculateTime={calculateTime}
+            trackIndex={trackIndex}
+            audioSrc={audioSrc}
+            durationTime={durationTime}
+            intervalRef={intervalRef}
+          />
           <div className="mt-10">
             {tracks.map((track, index) => {
               return (
@@ -175,26 +121,6 @@ function MusicPlayers({ tracks }): JSX.Element {
                   key={index}
                   className="flex items-center px-8 py-4 mx-10 mb-4 bg-white shadow-xl rounded-xl bg-opacity-40 bg-clip-padding backdrop-blur-sm hover:bg-opacity-60"
                 >
-                  {isPlaying ? (
-                    <button
-                      id={index}
-                      type="button"
-                      onClick={() => handlePlay(index)}
-                      className="text-[24px] text-white/40 hover:text-white/80"
-                    >
-                      <i className="fas">&#xf04c;</i>
-                    </button>
-                  ) : (
-                    <button
-                      id={index}
-                      type="button"
-                      onClick={() => handlePlay(index)}
-                      className="text-[24px] text-white/40 hover:text-white/80"
-                    >
-                      <i className="fas">&#xf04b;</i>
-                    </button>
-                  )}
-
                   <div className="w-0 h-10 ml-8 border border-solid border-white/60" />
                   <h2 className="flex ml-5 text-xl font-bold grow text-white/60 font-raleway">
                     {track.title}
