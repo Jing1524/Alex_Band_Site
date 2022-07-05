0dad01a441eb70a91139e32b4f074c216fbbc7fd
Jing Li <jingli1524@gmail.com>
2022-07-04 23:59:24 -0400

    mostly in place

--- components/About.tsx
+++ components/About.tsx
@@ -8,13 +8,13 @@ function About(): JSX.Element {
   return (
     <section
       id="about"
-      className="bg-gradient-to-r from-[#171717] to-black/90 flex flex-col 2xl:flex-row items-center space-x-0 2xl:space-x-10 py-16"
+      className="bg-gradient-to-r from-[#171717] to-black/90 flex flex-col xl:flex-row items-center space-x-0 2xl:space-x-10 pt-24"
     >
       <div className="flex flex-col ml-16 basis-1/2">
-        <h1 className="text-5xl text-white uppercase 2xl:text-6xl text-bold font-raleway">
+        <h1 className="text-5xl uppercase text-white/60 2xl:text-6xl text-bold font-raleway">
           about the project
         </h1>
-
+        <div className="mt-5 border border-2 border-solid border-white/60 w-80 md:w-[500px]" />
         <p className="text-xl 2xl:text-3xl text-white/60 font-raleway pr-16 2xl:pr-0 max-w-[100%] mt-10 leading-relaxed">
           I Live Dreaming is an album of collaboratively interpreted American
           standards and original compositions. Since meeting in New York City
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -8,7 +8,7 @@ function Contact(): JSX.Element {
   return (
     <section
       id="contact"
-      className="bg-gradient-to-r from-[#171717] to-black/90 flex flex-col 2xl:flex-row flex-col-reverse items-start space-x-10 py-16"
+      className=" bg-gradient-to-r from-[#171717] to-black/90 flex flex-col flex-col-reverse xl:flex-row items-start space-x-10 py-16"
     >
       <div className="w-[100vw] basis-3/5 pt-10">
         <Image
@@ -18,24 +18,28 @@ function Contact(): JSX.Element {
           width={1500}
         />
       </div>
-      <div className="flex flex-col items-start 2xl:items-end space-y-14 basis-2/5">
-        <h1 className="mr-32 text-6xl uppercase text-white/60 text-semibold font-raleway">
-          CONTACT
-        </h1>
+      <div className="flex flex-col items-start pl-6 xl:mt-8 xl:items-end space-y-14 basis-2/5">
+        <div className="flex flex-col items-start lg:items-end">
+          <h1 className="mr-32 text-5xl uppercase 2xl:text-6xl text-white/60 text-semibold font-raleway">
+            CONTACT
+          </h1>
+          <div className="mt-5 border border-2 border-solid mr-[130px] border-white/60 w-80 " />
+        </div>
+
         <div className="flex flex-col mr-32 space-y-10">
           <div className="flex flex-col items-start 2xl:items-end">
-            <h3 className="text-3xl text-white/60 font-raleway">
+            <h3 className="text-2xl 2xl:text-3xl text-white/60 font-raleway">
               RUSSELL KRANES
             </h3>
-            <h3 className="text-2xl text-white/60 font-raleway">
+            <h3 className="text-xl 2xl:text-2xl text-white/60 font-raleway">
               1kranes@gmail.com
             </h3>
           </div>
           <div className="flex flex-col items-start 2xl:items-end">
-            <h3 className="text-3xl text-white/60 font-raleway">
+            <h3 className="text-2xl 2xl:text-3xl text-white/60 font-raleway">
               RUSSELL KRANES
             </h3>
-            <h3 className="text-2xl text-white/60 font-raleway">
+            <h3 className="text-xl 2xl:text-2xl text-white/60 font-raleway">
               1kranes@gmail.com
             </h3>
           </div>
