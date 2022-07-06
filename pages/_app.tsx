f043620ada129a1534f349bce644e390d8731e61
Jing Li <jingli1524@gmail.com>
2022-07-06 00:02:55 -0400

    wahhh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { AppProps } from "next/app";
 
 import "../styles/index.css";
-import "@fortawesome/fontawesome-svg-core/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
