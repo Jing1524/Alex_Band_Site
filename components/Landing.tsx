3ca640e6c8cd2a4fd881f6bd5be7513c9862007c
Jing Li <jingli1524@gmail.com>
2022-07-05 22:35:44 -0400

    position fix

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -7,21 +7,24 @@ function Landing(): JSX.Element {
   const [navbarOpen, setNavbarOpen] = useState(false);
   const [isActive, setIsActive] = useState(false);
   return (
-    <section className="w-[100vw] h-auto 2xl:-mt-28">
-      <Image
-        src="/image/landing.jpg"
-        layout="responsive"
-        width={2048}
-        height={1536}
-      />
-      <div className="flex flex-col items-end text-right pr-10 md:pr-20 py-20 bg-black opacity-[91%] 2xl:absolute 2xl:bg-opacity-0 2xl:-mt-80 2xl:items-start 2xl:ml-16">
-        <h1 className="text-5xl font-bold text-gray-200 md:text-6xl lg:text-8xl font-raleway">
+    <section className="w-[100vw] h-auto xl:-mt-28">
+      <div className=" w-[100%] h-[100%]">
+        <Image
+          src="/image/landing.jpg"
+          layout="responsive"
+          width={2048}
+          height={1536}
+        />
+      </div>
+
+      <div className="flex flex-col items-end text-right pr-10 md:pr-20 py-20 bg-black opacity-[91%] xl:absolute xl:bg-opacity-0 xl:-mt-80 xl:items-start xl:ml-16">
+        <h1 className="text-5xl font-bold text-gray-200 md:text-6xl lg:text-6xl 2xl:text-8xl font-raleway">
           I LIVE DREAMING
         </h1>
-        <p className="hidden mt-10 text-3xl text-gray-200 uppercase font-raleway lg:block">
+        <p className="hidden mt-10 text-3xl text-gray-200 uppercase font-raleway xl:block">
           russell kranes, alex levine, sam weber, jay sawyer
         </p>
-        <div className="flex flex-col items-end mt-10 space-y-3 text-3xl text-gray-200 uppercase font-raleway lg:hidden">
+        <div className="flex flex-col items-end mt-10 space-y-3 text-3xl text-gray-200 uppercase font-raleway xl:hidden">
           <p>russell kranes</p>
           <p>alex levine</p>
           <p>sam weber</p>
