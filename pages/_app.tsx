bec3a52b234fcb17609f18035388a691b209c039
Jing Li <jingli1524@gmail.com>
2022-07-06 00:00:58 -0400

    import font awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AppProps } from "next/app";
 
 import "../styles/index.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
